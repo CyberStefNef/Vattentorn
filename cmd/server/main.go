92c035dcd05673982dd3e9f9006c6ce3ae2bb196
Stefan Müller <stef.mue02@gmail.com>
2024-10-12 18:35:20 +0200

    add infinity scrolling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -15,6 +17,21 @@ type Template struct {
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+type Image struct {
+	URL string
+	Alt string
+}
+
+var images = []Image{
+	{URL: "/assets/images/tower1.jpg", Alt: "Water Tower 1"},
+	{URL: "/assets/images/tower2.jpg", Alt: "Water Tower 2"},
+	{URL: "/assets/images/tower3.jpg", Alt: "Water Tower 3"},
+	{URL: "/assets/images/tower4.jpg", Alt: "Water Tower 4"},
+	{URL: "/assets/images/tower5.jpg", Alt: "Water Tower 5"},
+	{URL: "/assets/images/tower6.jpg", Alt: "Water Tower 6"},
+	{URL: "/assets/images/tower7.jpg", Alt: "Water Tower 7"},
 }
 
 func main() {
@@ -31,12 +48,32 @@ func main() {
 	e.Static("/assets", "web/assets") // Serve static assets
 	e.GET("/", HomeHandler)
 
+	e.GET("/images", func(c echo.Context) error {
+		// Get the "page" parameter (which page of images we're on)
+		page := c.QueryParam("page")
+		pageIndex, err := strconv.Atoi(page)
+		if err != nil || pageIndex < 0 {
+			pageIndex = 0 // Default to the first page if invalid
+		}
+
+		// Define how many images to load per "page"
+		pageSize := 3
+		start := pageIndex * pageSize
+		end := start + pageSize
+		if start >= len(images) {
+			return c.NoContent(http.StatusOK) // No more images to load
+		}
+		if end > len(images) {
+			end = len(images)
+		}
+
+		return c.Render(http.StatusOK, "images.html", images[start:end])
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func HomeHandler(c echo.Context) error {
-	data := map[string]interface{}{
-		"Title": "Welcome to My Project",
-	}
-	return c.Render(http.StatusOK, "index.html", data)
+	initialImages := images[:6]
+	return c.Render(http.StatusOK, "index.html", initialImages)
 }
